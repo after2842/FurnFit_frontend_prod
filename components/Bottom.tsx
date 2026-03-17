db310c001fdc421cd228c35c37b54dcca005eeae
Samuel <after2842@gmail.com>
2026-03-16 18:14:21 -0700

    footer

--- components/Bottom.tsx
+++ components/Bottom.tsx
@@ -4,9 +4,7 @@ const Bottom = () => {
     <div className="bottom-0 z-50 bg-white/50 backdrop-blur-md border-b border-slate-200">
       <div className="max-w-7xl mx-auto px-6 h-16 flex items-center text-center justify-between">
         <img src="/logo5.svg" alt="/>" width={36} className="text-slate-400" />
-        <div className="text-xs lg:text-sm">
-          This is for Winter 2026 CS125 Project
-        </div>
+        <div className="text-xs lg:text-sm">{""}</div>
         <div className="flex gap-4">
           <a
             href="#"
